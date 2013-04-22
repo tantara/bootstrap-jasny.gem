0cf8860f2f4554538a4e3cdf6c34dfb6fb6f6fcf
Taekmin Kim <tantara.tm@gmail.com>
2013-04-22 16:36:43 +0900

    Initial commit to bootstrap-jasny.

---
+++ test/test_bootstrap-jasny.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestBootstrapJasny < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
